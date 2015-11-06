636c88e8d2ba5d8af456e93243fdaf8563242959
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-11-06 10:43:37 -0500

    rough code for app and moods controller

---
+++ app/assets/javascripts/angular.js
@@ -0,0 +1,76 @@
+////////////////////////////////////////
+/////////// MOOD APPLICATION ///////////
+////////////////////////////////////////
+var app = angular.module('moodApp', []);
+
+
+////////////////////////////////////////
+/////////// SESSION CONTROLLER /////////
+////////////////////////////////////////
+
+// Verifies the current_user
+app.controller = ('SessionController', [$http, function(){
+  var controller = this;
+  $http.get('/session').success(function(data){
+    controller.current_user = data.current_user;
+  });
+}]);
+
+
+////////////////////////////////////////
+/////////// MOOD CONTROLLER ////////////
+////////////////////////////////////////
+app.controller = ('MoodController', [$http, function($http){
+
+  // setting up the authenticity token
+  var authenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+  var controller = this;
+
+  // Mood Integer values for HTML
+  this.happiness = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // GETS mood data and adds it to the controller
+  this.getMood = function(){
+    $http.get('/moods').success(function(data){
+      controller.current_user_moods = data.moods;
+    });
+  }
+
+
+
+  // grab mood data for current_user
+  this.getMood();
+
+  // CREATE a new mood post
+  this.createMood = function (){
+    // takes mood data from form and pushes it into the current_user_mood property
+    controller.current_user_moods.push({
+      happiness: this.newMoodInteger,
+      blurb: this.newMoodBlurb
+    });
+
+    // make the post to /moods
+    $http.post('/moods', {
+      authenticity_token: authenticity_token,
+      moods: {
+        happiness: this.newMoodInteger,
+        blurb: this.newMoodBlurb
+      }
+    }).success(function(data){
+      controller.current_user_moods.pop();
+      controller.current_user_moods.push(data.moods);
+      controller.getMood();
+    })
+  }
+  };
+
+  // GET THE MOOD NEW POST ROUTE
+  // $http.get('/moods').success(function(data){
+	// });
+
+}]);
+
+////////////////////////////////////////
+/////////// FACTOR CONTROLLER //////////
+////////////////////////////////////////
