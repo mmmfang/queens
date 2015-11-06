e8160209b418fcf37374f515ba6ecf1c10d5204a
shulihallak <shuli@invisiblenetworks.co>
2015-11-05 20:24:13 -0500

    welcome page form sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def created
+    @user = User.new(user_params)
+
+    if @user.save
+        flash[:message] = "Successfully created..."
+    else
+      flash[:message] = @user.errors.full_message.to_sentence
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+
+  end
 end
