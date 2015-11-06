3d603bdee49f7cd1a5c3360a2d8c3c8e2503658b
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-11-06 15:11:13 -0500

    linking angular to ruby

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -5,11 +5,11 @@ var app = angular.module('moodApp', []);
 
 
 ////////////////////////////////////////
-/////////// SESSION CONTROLLER /////////
+/////////// HEADER CONTROLLER //////////
 ////////////////////////////////////////
 
 // Verifies the current_user
-app.controller = ('SessionController', [$http, function(){
+app.controller('HeaderController', ['$http', function($http){
   var controller = this;
   $http.get('/session').success(function(data){
     controller.current_user = data.current_user;
@@ -20,67 +20,32 @@ app.controller = ('SessionController', [$http, function(){
 ////////////////////////////////////////
 /////////// MOOD CONTROLLER ////////////
 ////////////////////////////////////////
-app.controller = ('MoodController', [$http, function($http){
-
-  // setting up the authenticity token
-  var authenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-  var controller = this;
-
-  // Mood Integer values for HTML
-  this.happiness = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  // GETS mood data and adds it to the controller
-  this.getMood = function(){
-    $http.get('/moods').success(function(data){
-      controller.current_user_moods = data.moods;
-    });
-  };
-
-  // grab mood data for current_user
-  this.getMood();
-
-
-  // CREATE a new mood post
-  this.createMood = function(){
-
-    // takes mood data from form and pushes it into the current_user_mood property
-    controller.current_user_moods.push({
-      happiness: this.newHappiness
-    });
-
-    // make the post to /moods
-    $http.post('/moods', {
-      authenticity_token: authenticity_token,
-      moods: {
-        happiness: this.Happiness
-    }
-  }).success(function(data){
-    controller.current_user_moods.pop();
-    controller.current_user_moods.push(data.moods);
-    controller.getMood();
-  });
-  };
-}]);
-
-
-////////////////////////////////////////
-/////////// FACTOR CONTROLLER //////////
-////////////////////////////////////////
-app.controller('FactorController', ['$http', '$scope'], function($http, $scope){
-
-  // setting up the authenticity token
-  var authenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-  // creating the factor
-  this.createFactor = function(){
-    $http.post('/moods'+$scope.$parent.mood.id+'/factors', {
-      authenticity_token: authenticity_token,
-      factor: {
-        description: this.newFactorDescription
-      }
-    }).success(function(data){
-      $scope.$parent.factorsCtrl.getMood();
-    });
-  };
-});
+// app.controller('MoodController', ['$http', function($http){
+//
+//   // value of happiness determined by emoji picked; default is null
+//   this.happiness = null;
+//
+//   // get the mood value for current user
+//   this.newMood = function(){
+//
+//   };
+//
+//   // post the new mood
+//   this.postMood = function(){
+//
+//   };
+// }]);
+//
+// ////////////////////////////////////////
+// /////////// FACTOR CONTROLLER //////////
+// ////////////////////////////////////////
+// app.controller('FactorController', ['$http', function($http){
+//
+//   // value of happiness determined by emoji picked; default is null
+//   this.blurb = '';
+//
+//   // get the new blurb
+//
+//   // post the new blurb
+//
+// }]);
