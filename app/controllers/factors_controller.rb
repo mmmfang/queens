4dcf39655d612622963f2dd8d60fa3c35aadb8d2
shulihallak <shuli@invisiblenetworks.co>
2015-11-06 16:53:05 -0500

    factor controller updated

--- app/controllers/factors_controller.rb
+++ app/controllers/factors_controller.rb
@@ -1,2 +1,27 @@
 class FactorsController < ApplicationController
+
+  def create
+    factor = Factor.find(params[:mood_id])
+
+    @factor = factor.moods.new(factor_params)
+
+    if @factor.save
+
+    else
+      render json: {
+        error: {
+          message:
+          @factor.errors.full_messages.to_sentence
+        }
+      }
+  end
+end
+
+  private
+
+  def factor_params
+    params.require(:factor).permit(:blurb, :occurred_at)
+
+  end
+
 end
