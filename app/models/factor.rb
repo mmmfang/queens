8d9cce04ceb794d09c8ba76ba9330c4a025e152a
shulihallak <shuli@invisiblenetworks.co>
2015-11-07 14:59:33 -0500

    modifications for postman testing

--- app/models/factor.rb
+++ app/models/factor.rb
@@ -1,9 +1,10 @@
 class Factor < ActiveRecord::Base
 
-
   validates :blurb, presence: true,
   validates :mood, presence: true,
   validates :occurred_at, presence: true,
+
+  # paperclip image validation 
   validates_attachment :image, content_type: {
     content_type: ["image/jpeg", "image/gif", "image/png"]
   }
