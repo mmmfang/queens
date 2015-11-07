c5db3634887578bdf4e964aed6b8705d649897dc
shulihallak <shuli@invisiblenetworks.co>
2015-11-07 12:41:38 -0500

    db migration files with correct references and foreign ids delayed commit

--- db/migrate/20151106005540_create_moods.rb
+++ db/migrate/20151106005540_create_moods.rb
@@ -1,7 +1,7 @@
 class CreateMoods < ActiveRecord::Migration
   def change
     create_table :moods do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true #foreign_key user_id
       t.integer :happiness, null: false
 
       t.timestamps null: false
--- db/migrate/20151106195334_create_factors.rb
+++ db/migrate/20151106195334_create_factors.rb
@@ -1,7 +1,7 @@
 class CreateFactors < ActiveRecord::Migration
   def change
     create_table :factors do |t|
-      t.references :mood, index: true, foreign_key: true
+      t.references :mood, index: true, foreign_key: true #foreign_key mood_id
       t.text :blurb
 
       t.timestamps null: false
