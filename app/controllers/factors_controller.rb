2df4910e79ffed8f8a5fff6145f4e9b75498d400
shulihallak <shuli@invisiblenetworks.co>
2015-11-07 13:15:17 -0500

    factor controller updated as nested resource with all crud methods

--- app/controllers/factors_controller.rb
+++ app/controllers/factors_controller.rb
@@ -19,15 +19,32 @@ end
 
 def show
   mood = Mood.find(params[:mood_id])
-  @factor = mood.factor([:id])
+  @factor = mood.factors.find(factor_params)
 end
 
 def index
   mood = Mood.find(params[:mood_id])
   @factors = mood.factors
 end
-  private
 
+def edit
+  mood = Mood.find(params[:mood_id])
+  @factor = mood.factors.find(factor_params)
+end
+
+def update
+  mood = Mood.find(params[:mood_id])
+  @factor = mood.factors.find(factor_params)
+  @factor.save(factor_params)
+end
+
+def destroy
+  mood = Mood.find(params[:mood_id])
+  @factor = mood.factors.find(factor_params)
+  @factor.destroy
+end
+
+  private
   def factor_params
     params.require(:factor).permit(:blurb, :occurred_at)
 
