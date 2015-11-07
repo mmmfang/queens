b03605580cbbdeb4a13c0c70d3b91b77ceabe4e5
shulihallak <shuli@invisiblenetworks.co>
2015-11-07 13:38:47 -0500

    added paperclip migration and updated factor table to include photo attachment field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106195334) do
+ActiveRecord::Schema.define(version: 20151107183143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20151106195334) do
   create_table "factors", force: :cascade do |t|
     t.integer  "mood_id"
     t.text     "blurb"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "factors", ["mood_id"], name: "index_factors_on_mood_id", using: :btree
