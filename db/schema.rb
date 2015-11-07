c87e361d1b5b97094dbc3110ee7e38cb801c0d34
shulihallak <shuli@invisiblenetworks.co>
2015-11-07 14:08:18 -0500

    auto change in schema db after creating paperclip migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107183143) do
+ActiveRecord::Schema.define(version: 20151107185948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20151107183143) do
 
   create_table "moods", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "happiness",  null: false
+    t.integer  "happiness"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
