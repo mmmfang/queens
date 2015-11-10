5a5add17094ba2148dd4104747d0a2fe11dedfde
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-11-10 11:01:50 -0500

    styling changes

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -80,27 +80,27 @@ app.controller('MoodController', ['$http', function($http){
 
 
 //TESTING THE ROUTE CONTOLLER - Just put it in here for testing
-
-app.config([‘$routeProvider’, ‘$locationProvider’, function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode({enabled:true});
-
-  $routeProvider
-    .when('/moods',
-      { templateUrl: '/angular_templates/moods.html',  ///SHOW ALL PAGE
-        controller: 'MoodController',
-        controllerAs: 'mood'
-    }).when('/moods/:id',
-      { templateUrl: '/angular_templates/show.html',   ///SHOW ONE PAGE
-        controller:  'MoodController',
-        controllerAs: 'mood'
-    }).when('/users/:id',
-      { templateUrl: '/angular_templates/user.html',   ///SHOW ONE PAGE
-        controller:  'HeaderController',
-        controllerAs: 'header'
-    }).when('/chart',
-      { templateUrl: '/angular_templates/chart.html',  ///D3
-        controller:  'ChartController',
-        controllerAs: 'chart'
-    }).otherwise(
-      { redirectTo: '/' });
-});
+// 
+// app.config([‘$routeProvider’, ‘$locationProvider’, function($routeProvider, $locationProvider) {
+//   $locationProvider.html5Mode({enabled:true});
+//
+//   $routeProvider
+//     .when('/moods',
+//       { templateUrl: '/angular_templates/moods.html',  ///SHOW ALL PAGE
+//         controller: 'MoodController',
+//         controllerAs: 'mood'
+//     }).when('/moods/:id',
+//       { templateUrl: '/angular_templates/show.html',   ///SHOW ONE PAGE
+//         controller:  'MoodController',
+//         controllerAs: 'mood'
+//     }).when('/users/:id',
+//       { templateUrl: '/angular_templates/user.html',   ///SHOW ONE PAGE
+//         controller:  'HeaderController',
+//         controllerAs: 'header'
+//     }).when('/chart',
+//       { templateUrl: '/angular_templates/chart.html',  ///D3
+//         controller:  'ChartController',
+//         controllerAs: 'chart'
+//     }).otherwise(
+//       { redirectTo: '/' });
+// });
