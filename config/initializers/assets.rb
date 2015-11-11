57eda2bc8890de057d03e72435fc62ad98cf52ce
mmmfang <mae.fang@gmail.com>
2015-11-11 16:52:53 -0500

    update initializers/assets.rb file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( angular/app.js angular.js application.css 1.png 2.png 3.png 4.png 5.png 6.png 7.png 8.png 9.png 10.png)
+Rails.application.config.assets.precompile += %w( angular/app.js angular.js application.css .png)
